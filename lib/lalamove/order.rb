f600382f63e835ed7455fce97ef486e3133defe6
WR <wafiqrodzuan@gmail.com>
2020-07-01 21:53:49 +0800

    Parameter orders

--- lib/lalamove/order.rb
+++ lib/lalamove/order.rb
@@ -7,23 +7,23 @@ module Lalamove
     # Order 
     class Order
       def self.order(payload)
-        Helper.request('/v2/orders', payload, 'POST')
+        Helper.request('/v2/orders', 'POST', payload)
       end
 
       def self.order_detail(id)
-        Helper.request("/v2/orders/#{id}", nil, 'GET')
+        Helper.request("/v2/orders/#{id}", 'GET')
       end
 
       def self.driver_details(id, driver_id)
-        Helper.request("/v2/orders/#{id}/drivers/#{driver_id}", nil, 'GET')
+        Helper.request("/v2/orders/#{id}/drivers/#{driver_id}", 'GET')
       end
 
       def self.driver_location(id, driver_id)
-        Helper.request("/v2/orders/#{id}/drivers/#{driver_id}/location", nil, 'GET')
+        Helper.request("/v2/orders/#{id}/drivers/#{driver_id}/location", 'GET')
       end
 
       def self.cancel_order(id)
-        Helper.request("/v2/orders/#{id}/cancel", nil, 'PUT')
+        Helper.request("/v2/orders/#{id}/cancel", 'PUT')
       end
     end
   end
